2b27c3ac911fd6219a25772733903eaaeb1eedf1
sleepinglion(desktop) <admin@sleepinglion.pe.kr>
2025-01-25 18:05:49 +0900

    수정함:        app/models/user_authentication.rb

--- app/models/user_authentication.rb
+++ app/models/user_authentication.rb
@@ -1,7 +1,6 @@
 class UserAuthentication < ActiveRecord::Base
   belongs_to :user
   belongs_to :authentication_provider
-  serialize :params
 
   def self.create_from_omniauth(params, user, provider)
     token_expires_at = params['credentials']['expires_at'] ? Time.at(params['credentials']['expires_at']).to_datetime : nil
